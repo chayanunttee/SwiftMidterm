0bd9a65416def851016fc37e9469bae235d993f6
Admin <admin@MAC13.local>
2019-03-05 09:12:09 +0700

    Initial Commit

---
+++ dbmidterm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dbmidterm
+//
+//  Created by Admin on 5/3/2562 BE.
+//  Copyright © 2562 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
